7846a0e1a54421dc3c09391675ab53849eb48d18
Matt Ball <matt@matt-ball.co.uk>
2024-01-23 15:58:47 -0800

    no tests

--- test/src/identity.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (identity) {
-  describe('identity', function () {
-    it('should return what it is given', function () {
-      expect(identity('hello')).to.eql('hello')
-    })
-  })
-}
--- test/src/indexOf.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (indexOf) {
-  describe('indexOf', function () {
-  })
-}
